dd9f052ce3455f4ff640508144e1288189601509
Kut <kutuzov_a@list.ru>
2019-11-12 18:36:05 +0300

    Handlebars added

--- index.js
+++ index.js
@@ -2,17 +2,28 @@
 /* eslint-disable no-console */
 // const path = require('path')
 const express = require('express')
-const app = express()
+const exphbs = require('express-handlebars')
 
-app.use(express.static('public'))
+const app = express()
+const hbs = exphbs.create({
+  'defaultLayout': 'main',
+  'extname': 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+// eslint-disable-next-line no-process-env
+const PORT = process.env.PORT || 3000
+
+// app.use(express.static('public')) Need to remove???
 
 app.get('/', function (req, res) {
-  res.sendfile('index.html')
+  res.render('index')
 })
 
-app.listen(3000, function () {
-	console.log('Server is on 3000 port')
+app.listen(PORT, () => {
+	console.log(`Server is on ${PORT} port`)
 })
 
-console.log('Hello tutor!')
-console.log('Hello students too!')
